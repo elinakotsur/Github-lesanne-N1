ab8d77594d77b4642355b6be3c204e95620ab052
elinakotsur <elinakotsur9@gmail.com>
2025-05-08 14:07:23 +0300

    Salvestatud nimekiri staatusega 'alive' või 'dead'

--- characters.py
+++ characters.py
@@ -1,5 +1,11 @@
-file=open("characters.txt", "r", encoding="utf-8")
-for f in file:
-    print(f)
-    
-    
+import random
+
+with open("characters.txt", "r", encoding="utf-8") as file:
+    characters = file.readlines()
+
+with open("output.txt", "w", encoding="utf-8") as file2:
+    for name in characters:
+        status = random.choice(["alive", "dead"])
+        print(name.strip(), "-", status)
+        file2.write(f"{name.strip()} – {status}\n")
+        
