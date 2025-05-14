c9aa056f6c0fdb5d9eb73fc633e2a22d15648a51
elinakotsur <elinakotsur9@gmail.com>
2025-05-14 14:32:47 +0300

    Osa 2, lisa Meeleolu ja sõnumi generaator

--- Süvitsi mõistmine/emotions.py
+++ Süvitsi mõistmine/emotions.py
@@ -1,10 +1,42 @@
-from colorama import init, Fore, Style,Back
+from colorama import Fore, Style,Back
 import emoji
-init()
+     
 
 
 print(Back.CYAN +"Hello")
-mood=input("What is your mood now???(happy-1,sad-2,tired-3,shocked-4)")
+mood=input("What is your mood now???(happy,sad,tired,shocked)")
 
-if mood == 1:
-    print(Back.YELLOW +Fore.RED+"you are happy now!!))")
+moods = {
+    "happy": {
+        "font": Fore.BLACK,
+        "bg": Back.YELLOW,
+        "text": "You are happy right now!",
+        "enoji": emoji.emojize(':thumbs_up:')
+    },
+    "sad": {
+        "font": Fore.WHITE,
+        "bg": Back.BLUE,
+        "text": "You are sad right now",
+        "enoji": emoji.emojize(':disappointed_face:')
+    },
+    "tired": {
+        "font": Fore.BLACK,
+        "bg": Back.GREEN,
+        "text": "You are tirda right now",
+        "enoji": emoji.emojize(':sleeping_face:')
+    },
+    "shocked": {
+        "font": Fore.WHITE,
+        "bg": Back.MAGENTA,
+        "text": "You are shocked right now",
+        "enoji": emoji.emojize(':fearful_face:')
+    }
+}
+
+
+if mood in moods:
+    m = moods[mood]
+    print(m["bg"] + m["font"] +  " " + m["text"]+" " + m["enoji"] )
+
+else:
+    print(Fore.RED + "Something was wrong")
